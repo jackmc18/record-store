9c149fa3413045415a503e1dd75d084a0e0109ec
Jackmc <jackmc1122@gmail.com>
2019-04-30 16:21:44 -0700

    Fix record api test title.

--- test/record-api.test.js
+++ test/record-api.test.js
@@ -3,7 +3,7 @@ import recordApi from '../src/record-api.js';
 const test = QUnit.test;
 QUnit.module('record api');
 
-test('Test a normal record entry with no rating expect no ratingScore property', assert => {
+test('Test saving and getting a single record', assert => {
   //Arrange
   // Set up your parameters and expectations
   const record = { artistName: 'Nick Hakim' };

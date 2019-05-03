128c84af5b66351a6753de2aec22c0df5c6c0268
Jackmc <jackmc1122@gmail.com>
2019-05-02 17:45:26 -0700

    Refactor cell functions.

--- src/records-list.js
+++ src/records-list.js
@@ -8,58 +8,68 @@ for(let i = 0; i < records.length; i++) {
   const record = records[i];
   const tr = document.createElement('tr');
 
-  const link = document.createElement('a');
+  const titleCell = makeTitleCell(record.albumTitle);
+  tr.appendChild(titleCell);
 
+  const artistCell = makeTextCell(record.artistName);
+  tr.appendChild(artistCell);
+
+  const catalogNumCell = makeTextCell(record.catalogNumber);
+  tr.appendChild(catalogNumCell);
+
+  const colorCell = makeTextCell(record.recordColor);
+  tr.appendChild(colorCell);
+
+  const rmpCell = makeTextCell(record.recordRPM);
+  tr.appendChild(rmpCell);
+
+  const conditionCell = makeTextCell(record.recordCondition);
+  tr.appendChild(conditionCell);
+
+  const jacketCell = makeTextCell(record.jacketCondition);
+  tr.appendChild(jacketCell);
+
+  const genreCell = makeListCell(record.recordGenre);
+  tr.appendChild(genreCell);
+
+  const ratingCell = makeRatingCell(record.ratingScore);
+  tr.appendChild(ratingCell);
+
+  tbody.appendChild(tr);
+}
+
+function makeListCell(list) {
+  let listString = '';
+  if(list) {
+    listString = list.join(', ');
+  }
+  return makeTextCell(listString);
+}
+
+function makeRatingCell(rating) {
+  let ratingString = '';
+  if(rating) {
+    ratingString = rating;
+  } else {
+    ratingString = 'none';
+  }
+  return makeTextCell(ratingString);
+}
+
+function makeTitleCell(albumTitle) {
+  const link = document.createElement('a');
   const searchParams = new URLSearchParams();
-  searchParams.set('albumTitle', record.albumTitle);
-  link.textContent = record.albumTitle;
+  searchParams.set('albumTitle', albumTitle);
+  link.textContent = albumTitle;
   link.href = 'record-detail.html?' + searchParams.toString();
 
   const titleCell = document.createElement('td');
   titleCell.appendChild(link);
-  tr.appendChild(titleCell);
+  return titleCell;
+}
 
-  const artistCell = document.createElement('td');
-  artistCell.textContent = record.artistName;
-  tr.appendChild(artistCell);
-
-  const catalogNumCell = document.createElement('td');
-  catalogNumCell.textContent = record.catalogNumber;
-  tr.appendChild(catalogNumCell);
-
-  const colorCell = document.createElement('td');
-  colorCell.textContent = record.recordColor;
-  tr.appendChild(colorCell);
-
-  const rmpCell = document.createElement('td');
-  rmpCell.textContent = record.recordRPM;
-  tr.appendChild(rmpCell);
-
-  const conditionCell = document.createElement('td');
-  conditionCell.textContent = record.recordCondition;
-  tr.appendChild(conditionCell);
-
-  const jacketCell = document.createElement('td');
-  jacketCell.textContent = record.jacketCondition;
-  tr.appendChild(jacketCell);
-
-  const genreCell = document.createElement('td');
-  let genreList = '';
-  if(record.recordGenre) {
-    genreList = record.recordGenre.join(', ');
-  }
-  genreCell.textContent = genreList;
-  tr.appendChild(genreCell);
-
-  const ratingCell = document.createElement('td');
-  let rating = '';
-  if(record.ratingScore) {
-    rating = record.ratingScore;
-  } else {
-    rating = 'none';
-  }
-  ratingCell.textContent = rating;
-  tr.appendChild(ratingCell);
-
-  tbody.appendChild(tr);
+function makeTextCell(text) {
+  const textCell = document.createElement('td');
+  textCell.textContent = text;
+  return textCell;
 }

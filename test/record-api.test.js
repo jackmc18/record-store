58c24133f5c7a9c7fb1b06c79770831b5772f480
Jackmc <jackmc1122@gmail.com>
2019-05-02 17:48:09 -0700

    Fix failing test by replacing 'records' with key.

--- test/record-api.test.js
+++ test/record-api.test.js
@@ -7,7 +7,7 @@ const key = 'test-records';
 recordApi.key = key;
 
 test('Test saving two records and get the second one back', assert => {
-  localStorage.removeItem('records');
+  localStorage.removeItem(key);
   //Arrange
   // Set up your parameters and expectations
   const record1 = { albumTitle: 'Where Will We Go Part 1 & 2' };
@@ -22,7 +22,7 @@ test('Test saving two records and get the second one back', assert => {
 });
 
 test('Test for nothing stored get an empty array back', assert => {
-  localStorage.removeItem('records');
+  localStorage.removeItem(key);
   //Arrange
   // Set up your parameters and expectations
   const expected = [];
@@ -34,7 +34,7 @@ test('Test for nothing stored get an empty array back', assert => {
 });
 
 test('Test 2 records in storage return 2 records', assert => {
-  localStorage.removeItem('records');
+  localStorage.removeItem(key);
   //Arrange
   // Set up your parameters and expectations
   const record1 = { record: 'Wish You Were Here' };

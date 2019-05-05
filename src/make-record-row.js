5be8c3650a05a369b0176be58a860862772706b0
Jackmc <jackmc1122@gmail.com>
2019-05-05 15:47:00 -0700

    Move refactored functions into their own makeRecordRow file.

---
+++ src/make-record-row.js
@@ -0,0 +1,44 @@
+function makeListCell(list) {
+  let listString = '';
+  if(list) {
+    listString = list.join(', ');
+  }
+  return makeTextCell(listString);
+}
+
+function makeRatingCell(rating) {
+  let ratingString = '';
+  if(rating) {
+    ratingString = rating;
+  } else {
+    ratingString = 'none';
+  }
+  return makeTextCell(ratingString);
+}
+
+function makeTitleCell(albumTitle) {
+  const link = document.createElement('a');
+  const searchParams = new URLSearchParams();
+  searchParams.set('albumTitle', albumTitle);
+  link.textContent = albumTitle;
+  link.href = 'record-detail.html?' + searchParams.toString();
+
+  const titleCell = document.createElement('td');
+  titleCell.appendChild(link);
+  return titleCell;
+}
+
+function makeTextCell(text) {
+  const textCell = document.createElement('td');
+  textCell.textContent = text;
+  return textCell;
+}
+
+const makeRecordRow = {
+  makeListCell,
+  makeRatingCell,
+  makeTitleCell,
+  makeTextCell
+};
+
+export default makeRecordRow;
--- src/records-list.js
+++ src/records-list.js
@@ -1,4 +1,5 @@
 import recordApi from './record-api.js';
+import makeRecordRow from './make-record-row.js';
 
 const tbody = document.getElementById('records');
 
@@ -8,68 +9,32 @@ for(let i = 0; i < records.length; i++) {
   const record = records[i];
   const tr = document.createElement('tr');
 
-  const titleCell = makeTitleCell(record.albumTitle);
+  const titleCell = makeRecordRow.makeTitleCell(record.albumTitle);
   tr.appendChild(titleCell);
 
-  const artistCell = makeTextCell(record.artistName);
+  const artistCell = makeRecordRow.makeTextCell(record.artistName);
   tr.appendChild(artistCell);
 
-  const catalogNumCell = makeTextCell(record.catalogNumber);
+  const catalogNumCell = makeRecordRow.makeTextCell(record.catalogNumber);
   tr.appendChild(catalogNumCell);
 
-  const colorCell = makeTextCell(record.recordColor);
+  const colorCell = makeRecordRow.makeTextCell(record.recordColor);
   tr.appendChild(colorCell);
 
-  const rmpCell = makeTextCell(record.recordRPM);
+  const rmpCell = makeRecordRow.makeTextCell(record.recordRPM);
   tr.appendChild(rmpCell);
 
-  const conditionCell = makeTextCell(record.recordCondition);
+  const conditionCell = makeRecordRow.makeTextCell(record.recordCondition);
   tr.appendChild(conditionCell);
 
-  const jacketCell = makeTextCell(record.jacketCondition);
+  const jacketCell = makeRecordRow.makeTextCell(record.jacketCondition);
   tr.appendChild(jacketCell);
 
-  const genreCell = makeListCell(record.recordGenre);
+  const genreCell = makeRecordRow.makeListCell(record.recordGenre);
   tr.appendChild(genreCell);
 
-  const ratingCell = makeRatingCell(record.ratingScore);
+  const ratingCell = makeRecordRow.makeRatingCell(record.ratingScore);
   tr.appendChild(ratingCell);
 
   tbody.appendChild(tr);
-}
-
-function makeListCell(list) {
-  let listString = '';
-  if(list) {
-    listString = list.join(', ');
-  }
-  return makeTextCell(listString);
-}
-
-function makeRatingCell(rating) {
-  let ratingString = '';
-  if(rating) {
-    ratingString = rating;
-  } else {
-    ratingString = 'none';
-  }
-  return makeTextCell(ratingString);
-}
-
-function makeTitleCell(albumTitle) {
-  const link = document.createElement('a');
-  const searchParams = new URLSearchParams();
-  searchParams.set('albumTitle', albumTitle);
-  link.textContent = albumTitle;
-  link.href = 'record-detail.html?' + searchParams.toString();
-
-  const titleCell = document.createElement('td');
-  titleCell.appendChild(link);
-  return titleCell;
-}
-
-function makeTextCell(text) {
-  const textCell = document.createElement('td');
-  textCell.textContent = text;
-  return textCell;
 }
